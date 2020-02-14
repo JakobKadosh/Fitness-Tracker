564d40c4f9e7c337e84c2d62813ffa374f197069
Yaakov Kadosh <43819536+JakobKadosh@users.noreply.github.com>
2020-02-14 16:12:16 +0200

    API safe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyBWMV-50XdQfnrulC4cNET0cHKXbHb-e8w',
-    authDomain: 'ng-fitness-tracker-18298.firebaseapp.com',
-    databaseURL: 'https://ng-fitness-tracker-18298.firebaseio.com',
-    projectId: 'ng-fitness-tracker-18298',
-    storageBucket: 'ng-fitness-tracker-18298.appspot.com',
-    messagingSenderId: '1018713577324',
-    appId: '1:1018713577324:web:d53c0312017abce9dfc549'
-  }
+
 };
 
 /*
